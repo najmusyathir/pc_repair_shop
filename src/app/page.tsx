39b626bf54391f6f992f1ca24551eb67876070c8
Najmu Syathir <134459622+najmusyathir@users.noreply.github.com>
2025-06-14 18:32:44 +0800

    Modify: Fix id datatype & separate PanelCard component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
+import PanelCard from "@/components/PanelCard";
 import StatusBadge from "@/components/StatusBadge";
 import { useState } from "react";
 
 interface RepairRecord {
-  id: number | string;
+  id: number;
   device: string;
   date: string;
   status: string;
@@ -129,30 +130,4 @@ export default function Home() {
   );
 }
 
-function PanelCard({
-  title,
-  description,
-  href,
-  color,
-}: {
-  title: string;
-  description: string;
-  href: string;
-  color: "blue" | "green";
-}) {
-  const bgColor =
-    color === "blue"
-      ? "bg-blue-600 hover:bg-blue-700"
-      : "bg-green-600 hover:bg-green-700";
-  return (
-    <div className="bg-white shadow-lg rounded-2xl p-8">
-      <h2 className="text-2xl font-semibold mb-2">{title}</h2>
-      <p className="text-gray-600 mb-4">{description}</p>
-      <a
-        href={href}
-        className={`inline-block mt-2 px-5 py-2 rounded text-white transition ${bgColor}`}>
-        {title.split(" ")[0]} Login
-      </a>
-    </div>
-  );
-}
+
